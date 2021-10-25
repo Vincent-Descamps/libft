80553aa0e167c4665ffe1050284c8107ae01b2d4
Vincent Descamps <vincedescamps@gmail.com>
2021-10-25 23:08:16 +0200

    dtrlcpy

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: vdescamp <vdescamp@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 12:34:12 by vdescamp          #+#    #+#             */
-/*   Updated: 2021/10/22 17:49:14 by vdescamp         ###   ########.fr       */
+/*   Updated: 2021/10/22 20:08:32 by vdescamp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,16 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	size_t	i;
 
 	i = 0;
-	if (!dst || !src)
-		return (0);
-	while (src[i] != '\0')
+	if (dstsize > 0)
+	{
+		while (src[i] && i > dstsize - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	while (src[i])
 		i++;
-	if (dstsize == 0)
-		return (i);
-
-
+	return (i);
 }
