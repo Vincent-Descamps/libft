31342790b32f343d5ec9a665b074ec98545bbacf
Vincent Descamps <vincedescamps@gmail.com>
2021-10-26 23:52:44 +0200

    debut strtrim

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -1,50 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vdescamp <vdescamp@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/25 14:45:29 by vdescamp          #+#    #+#             */
-/*   Updated: 2021/10/26 18:45:04 by vdescamp         ###   ########.fr       */
+/*   Created: 2021/10/26 22:59:14 by vdescamp          #+#    #+#             */
+/*   Updated: 2021/10/26 23:51:42 by vdescamp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_strtrim(char const *s1, char const set)
 {
-	size_t	i;
-	size_t	j;
-	char	*str;
+	int		i;
+	int		j;
+	char	*s1_cpy;
 
 	i = 0;
 	j = 0;
-	if (!s)
-		return (NULL);
-	str = (char *)malloc(sizeof(char) * (len + 1));
-	if (!str)
-		return (NULL);
-	while (s[i])
+	s1_cpy = (char *)malloc(sizeof(char) * (ft_strlen(s1) - ???));
+	while (s1[i] != '\0')
 	{
-		if (i >= start && j < len)
+		if (s1[i] == set)
+			i++;
+		while (s1[i] != set)
 		{
-			str[j] = s[i];
+			s1_cpy[j] = s1[i];
 			j++;
 		}
-		i++;
 	}
-	str[j] = '\0';
-	return (str);
+	return (s1_cpy);
 }
-/*
+
 int	main(void)
 {
-	char	str[] = "Salut les potos";
-	unsigned	int start = 1;
-	size_t	len = 3;
+	char s1[] = "	Jusqu'ici tout va bien.	";
+	char set = '\t';
 
-	//printf("%s\n", str);
-	printf("%s", ft_substr(str, start, len));
+	printf("%s", ft_strtrim(s1, set));
 }
-*/
